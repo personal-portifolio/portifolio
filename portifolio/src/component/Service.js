f404496dc4173cbd172d089dc53e19563f865335
Bereket Yosef <134799825+brkt1@users.noreply.github.com>
2024-11-12 17:28:10 +0300

    hero page content add

--- portifolio/src/component/Service.js
+++ portifolio/src/component/Service.js
@@ -4,20 +4,20 @@ function Service() {
   return (
     <div>
       <div>
-        <h1 className=' mx-auto mt-24 text-center  text-teal-400 text-xl font-semibold'>Service Provided By Me.</h1>
+        <h1 className=' mx-auto mt-24 text-center  text-teal-400 text-xl font-semibold'>Service Provided By Me</h1>
      </div>
      <div className='flex justify-around mt-7 text-center  items-center '>
      <div className='flex flex-col items-center'>
         <img className='text-teal-400 w-12' src='image/coding.png'></img>
-           <p className='text-gray-400 text-sm '>Web Development</p> 
+           <p className='text-gray-400 my-4 text-sm '>Web Development</p> 
         </div>
         <div className='flex flex-col items-center'>
         <img className='text-teal-400 w-12' src='image/illustration.png'></img>
-           <p className='text-gray-400 text-sm'>UI/UX Design</p> 
+           <p className='text-gray-400 my-4 text-sm'>UI/UX Design</p> 
         </div>
         <div className='flex flex-col items-center'>
         <img className='text-teal-400 w-12' src='image/image.png'></img>
-           <p className='text-gray-400 text-sm'>Graphic Design</p> 
+           <p className='text-gray-400 my-4 text-sm'>Graphic Design</p> 
         </div>
      </div>
     </div>
